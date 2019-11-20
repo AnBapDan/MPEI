aaec05482fc10dd69e818f48e2403e305e7d2878
Daniel <dani.andrade@ua.pt>
2019-11-20 18:40:04 +0000

    Ideias iniciais

---
+++ src/LoadData.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class LoadData {
+	
+	private static void loadInfo(File f) throws IOException {
+		Scanner lFile = new Scanner(f);
+		while(lFile.hasNextLine()) {
+			String[] tmp = lFile.nextLine().split("/");				//Divide o nome da lista de compras
+			
+			String[] prod = tmp[1].split(",");
+			for(int i =0; i<prod.length; i++ ) {
+				String[] d= prod[i].split("#");
+				Produto f = new Produto(d[0],d[1],Integer.parseInt(d[2]));
+				if()
+				
+			}
+		}
+		Utilizador a = new Utilizador();
+		
+	}
+}
--- src/Utilizador.java
+++ src/Utilizador.java
@@ -1,19 +1,15 @@
+import java.util.ArrayList;
 
 public class Utilizador {
 
-	private int id;
 	private String nome;
 	private ArrayList<Produto> produtos;
 	
 	public Utilizador(int id, String nome) {
-		this.id = id;
 		this.nome = nome;
 		produtos = new ArrayList<Produto>();
 	}
 	
-	public int getID() {
-		return id;
-	}
 	
 	public String getNome() {
 		return nome;
