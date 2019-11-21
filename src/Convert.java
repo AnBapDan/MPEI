9100dc048ddb0b3cd9a94266a609753a435cfe45
Daniel <dani.andrade@ua.pt>
2019-11-21 15:04:00 +0000

    Added Convert + MinHash

---
+++ src/Convert.java
@@ -0,0 +1,21 @@
+
+public class Convert {
+	private Utilizador a;
+	private Produto p;
+	private String prod;
+	private Tipo t;
+	private double preco;
+	
+	public Convert(Utilizador a) {
+		this.a =a ;
+		
+	}
+	
+	private void toNumber(Produto p) {
+		this.p = p;
+		prod = p.getNome();
+		t = p.getTipo().getVal();
+		preco = p.getPreco();
+		
+	}
+}
