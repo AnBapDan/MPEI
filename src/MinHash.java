50beeb3402ff89b87f3e7d06452a1f898377af8f
Daniel <dani.andrade@ua.pt>
2019-11-21 15:04:11 +0000

    Update

--- src/MinHash.java
+++ src/MinHash.java
@@ -1,4 +1,28 @@
 
 public class MinHash {
-
+	
+	private double threshold;
+	private int numusers;
+	
+	public MinHash(double threshold, int numusers) {
+		this.threshold= threshold;
+		
+	}
+	
+	
+	public static void main() {
+		users
+	}
+	
+	private  double[][][] getMatrix() {
+		
+		
+		
+		return null;
+	}
+	
+	
+	
+	
+	
 }
