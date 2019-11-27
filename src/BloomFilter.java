250bfac3f2a1060ff1edb42ab5509b2b840e7675
Francisco Monteiro <francisco.monteiro@ua.pt>
2019-11-27 19:10:29 +0000

    Update Bloom

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class BloomFilter {
@@ -6,43 +7,34 @@ public class BloomFilter {
 	private static ArrayList<Produto> allProds;
 	private static ArrayList<Utilizador> allUsers;
 	private static HashMap<Utilizador,ArrayList<Produto>> listagem;
+	private static int [][] matrix;
 	
 	public BloomFilter() {
 		allProds = new ArrayList<Produto>();
 		listagem = new HashMap<Utilizador,ArrayList<Produto>>();  //Hashmap com todas as compras (Utilizador - Produto)
+		allUsers = new ArrayList<Utilizador>();
+		matrix = new int[1000][1000];
 	}
 	
-	public static void add(Utilizador u, Produto p) {
-		if(!(exists(u,p))) {
-			if(listagem.containsKey(u)) {
-				ArrayList<Produto> tmp = listagem.get(u);
-				if(!(tmp.contains(p))) {
-					tmp.add(p);
-				}
-				listagem.replace(u,tmp);
-			} else {
-				ArrayList<Produto> values = new ArrayList<Produto>();
-				values.add(p);
-				listagem.put(u, values);
-			}
+	public static void add(Utilizador u, Produto[] p) {
+		int user = u.hashCode();
+		int [] added = hashFunc(user);
+		
+	}
+	
+	private static int[] hashFunc(int seed) {    //h1(1) = mod(sum(str(1,:).*h(1:),M);  M = 1009; h = randi(100,M,3); str = ('ola','ela','ele','ggg');
+		int [] p = new int[1009];
+		
+		for(int i = 0; i < allProds.size(); i++) {
+			int hashCode = (allProds.get(i).getID() * (int)(Math.random()*seed)) % 1009;
+			p[i] = hashCode; 
 		}
-	}
-	
-	public static boolean exists(Utilizador u, Produto p) {
-		return false;
+		return p;
 	}
 
 	public static HashMap<Utilizador,ArrayList<Produto>> getListagem() {
 		return listagem;
 	}
-
-//	public static ArrayList<Produto> getAllProds() {
-//		return allProds;
-//	}
-//	
-//	public static ArrayList<Utilizador> getAllUsers() {
-//		return allUsers;
-//	}
 	
 	public static ArrayList<Produto> getSingleList(Utilizador u) {
 		return u.getProdutos();
