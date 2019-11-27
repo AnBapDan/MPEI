268c7093094623ce6f2c7dce54bdb081905a91cb
Francisco Monteiro <francisco.monteiro@ua.pt>
2019-11-27 21:36:37 +0000

    Updates Main

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,42 +1,32 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class BloomFilter {
 
-	private static ArrayList<Produto> allProds;
-	private static ArrayList<Utilizador> allUsers;
-	private static HashMap<Utilizador,ArrayList<Produto>> listagem;
+//	private static ArrayList<Produto> allProds;
+//	private static ArrayList<Utilizador> allUsers;
+//	private static HashMap<Utilizador,ArrayList<Produto>> listagem;
 	private static int [][] matrix;
 	
 	public BloomFilter() {
-		allProds = new ArrayList<Produto>();
-		listagem = new HashMap<Utilizador,ArrayList<Produto>>();  //Hashmap com todas as compras (Utilizador - Produto)
-		allUsers = new ArrayList<Utilizador>();
+//		allProds = new ArrayList<Produto>();
+//		listagem = new HashMap<Utilizador,ArrayList<Produto>>();  //Hashmap com todas as compras (Utilizador - Produto)
+//		allUsers = new ArrayList<Utilizador>();
 		matrix = new int[1000][1000];
 	}
 	
-	public static void add(Utilizador u, Produto[] p) {
-		int user = u.hashCode();
-		int [] added = hashFunc(user);
+	public static void add(int u, int p) {
+		int [] added = hashFunc(p,u);
 		
 	}
 	
-	private static int[] hashFunc(int seed) {    //h1(1) = mod(sum(str(1,:).*h(1:),M);  M = 1009; h = randi(100,M,3); str = ('ola','ela','ele','ggg');
-		int [] p = new int[1009];
+	private static int[] hashFunc(int p, int u) {    //h1(1) = mod(sum(str(1,:).*h(1:),M);  M = 1009; h = randi(100,M,3); str = ('ola','ela','ele','ggg');
+		int [] marked = new int[1009];
 		
-		for(int i = 0; i < allProds.size(); i++) {
-			int hashCode = (allProds.get(i).getID() * (int)(Math.random()*seed)) % 1009;
-			p[i] = hashCode; 
+		for(int i = 0; i < matrix.length; i++) {
+			int hashCode = (matrix[u][i] * (int)(Math.random()*p)) % 1009;
+			marked[i] = hashCode;
 		}
-		return p;
-	}
-
-	public static HashMap<Utilizador,ArrayList<Produto>> getListagem() {
-		return listagem;
-	}
-	
-	public static ArrayList<Produto> getSingleList(Utilizador u) {
-		return u.getProdutos();
+		return marked;
 	}
 }
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BloomFilter bf = new BloomFilter();
+		MinHash mh = new MinHash();
+		
+		
+		Path f = Paths.get("src/db.txt");
+		Scanner scf = new Scanner(f);
+		while(scf.hasNextLine())
+			mh.newline(scf.nextLine());
+		
+//		List<String> list = Files.readAllLines(f);
+//		Iterator<String> it = list.iterator();
+//		int user,prod;
+//		List<Object> users = new ArrayList<Object>(1000);
+
+//		while(it.hasNext()) {
+//			String[] t = it.next().split("\t");
+//			user = Integer.parseInt(t[0]);
+//			prod = Integer.parseInt(t[1]);
+//			try {
+//				ArrayList<Integer> tmp = new ArrayList<Integer>(100);	
+//				tmp.addAll((Collection<? extends Integer>) users.get(user-1));
+//				tmp.add(prod);
+//				users.add(user-1,tmp);				
+//			}catch(Exception e) {
+//				ArrayList<Integer> tmp = new ArrayList<Integer>(100);
+//				tmp.add(prod);
+//				users.add(user-1,tmp);	
+//			}
+//
+//		}
+	}
+}
