ce471b3f4093104eb635ed80de4e646ed8f58d4e
Daniel <dani.andrade@ua.pt>
2019-11-27 17:11:03 +0000

    Clear

--- src/MinHash.java
+++ src/MinHash.java
@@ -2,25 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MinHash {
-	private HashMap<Utilizador,ArrayList<Produto>> intern;
-	private String n;
-	
-	public MinHash() {
-		intern = BloomFilter.getListagem();
-
-		
-		
-	}
-	
-	public double similarity() {
-		double value =0;
-		for(int i=0 ; i<intern.size() & intern.containsKey(i); i++) {
-			
-		}
-		
-		
-		return value;
-	}
 	
 	
 	
