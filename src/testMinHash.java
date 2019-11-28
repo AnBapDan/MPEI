0bfb1e49c8cd508679a34d7fbddb69bd40b9972f
Francisco Monteiro <francisco.monteiro@ua.pt>
2019-11-28 14:59:11 +0000

    Teste MinHash(semi-final)

--- src/testMinHash.java
+++ src/testMinHash.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,17 +11,22 @@ public class testMinHash {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		Path f = Paths.get("src/testMH.txt");
+		Path f = Paths.get("src/db.txt");
 		Scanner scf = new Scanner(f);
 		List<String> lines = Files.readAllLines(f);
 		MinHash mh = new MinHash(lines);
 		
+		
+		PrintWriter pw = new PrintWriter("src/pw.txt");
+		
 		double[][] tmp = mh.getMatrix();
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				System.out.print(tmp[i][j]+" ");
+		for(int i = 0; i < mh.getNl(); i++) {
+			for(int j = 0; j < mh.getNl(); j++) {
+//				System.out.print(tmp[i][j]+" ");
+				pw.print(tmp[i][j]+" ");
 			}
-			System.out.println();
+//			System.out.println();
+			pw.println();
 		}
 	}
 
