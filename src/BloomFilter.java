dcd71820fa149233572645e49bc23ffcfd6c8a3c
Francisco Monteiro <francisco.monteiro@ua.pt>
2019-12-02 15:48:30 +0000

    BloomFilter update

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,25 +1,23 @@
 
 public class BloomFilter {
 
-	private static int [][] matrix;
-	private static int[] marked;
+	private static int[] bloom;
 	
 	public BloomFilter() {
-		matrix = new int[1000][1000];
-		marked = new int[1009];
+		bloom = new int[1009];
 	}
 	
 	public static void add(int u, int p) {
-		int [] added = hashFunc(u,p);
+		int [] added = exists(u,p);
 		
 	}
 	
-	private static int[] hashFunc(int u, int p) {  //h1(1) = mod(sum(str(1,:).*h(1:),M);  M = 1009; h = randi(100,M,3); str = ('ola','ela','ele','ggg');
+	private static int[] exists(int u, int p) {  //h1(1) = mod(sum(str(1,:).*h(1:),M);  M = 1009; h = randi(100,M,3); str = ('ola','ela','ele','ggg');
 		
-		for(int i = 0; i < matrix.length; i++) {
-			int hashCode = (matrix[u][i] * (int)(Math.random()*p)) % 1009;
-			marked[hashCode] = 1;
+		for(int i = 0; i < bloom.length; i++) {
+			int hashCode = (int)(Math.random()*p)) % 1009;
+			bloom[hashCode] = 1;
 		}
-		return marked;
+		return bloom;
 	}
 }
